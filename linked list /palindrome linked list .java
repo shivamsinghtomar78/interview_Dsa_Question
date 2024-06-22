1886003773da8031cba174ee021f66190354430e
shivamsinghtomar78 <139133103+shivamsinghtomar78@users.noreply.github.com>
2024-06-22 12:26:54 +0530

    Update palindrome linked list .java

--- linked list /palindrome linked list .java
+++ linked list /palindrome linked list .java
@@ -80,3 +80,80 @@ class Solution {
         
     }
 }
+
+
+// Approach 3:convert in arrayList and compare element from starting and end 
+// T.C : O(n)
+// S.C : O(n)
+class solution{
+    public boolean Ispalindrome(ListNode head){
+        List<Integer> temp=new ArrayList();
+        ListNode curr=head;
+        while(curr!=null){
+            temp.add(curr.val);
+            curr=curr.next;
+        }
+        int i=0;
+        int j=temp.size()-1;
+        while(i<j){
+            if(!temp.get(i).equals(temp.get(i)))
+                return false;
+            i++;
+            j--;
+        }
+        return true;
+    }
+}
+
+
+// Approach-4:Travel in List with slow and fast and reverse first with travel and compare first half and second half
+// T.C : O(n)
+// S.C : O(1)
+class solution{
+    public boolean Ispalindrome(ListNode head){
+        ListNode slow=head;
+        ListNode fast=Head;
+        ListNode prev=null;
+        while(fast!=null && fast.next!=null){
+            fast=fast.next.next;
+            ListNode temp=slow.next;
+            slow.next=prev;
+            prev=slow;
+            slow=temp;
+        }
+        if(fast!=null){
+            slow=slow.next;
+        }
+        while(slow!=null && prev!=null){
+            if(slow.val!=prev.val) return false;
+            prev=prev.next;
+            slow=slow.next;
+        }
+        return true;
+    }
+}
+
+// Approach-5 : using recursion only   
+// T.C : O(n)
+// S.C : O(1) Auxiliary space
+
+
+class Solution {
+    ListNode curr;
+
+    public boolean solve(ListNode head) {
+        if(head == null)
+        return true;
+        boolean ans=solve(head.next);
+        if(head.val!=curr.val) return false; 
+        curr = curr.next;
+        return ans;
+    }
+
+    public boolean isPalindrome(ListNode head) {
+        curr = head;
+
+        return solve(head);
+    }
+}
+
