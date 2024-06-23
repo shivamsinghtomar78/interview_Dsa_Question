3df904c4a92761752dc9c34a278b475078ba2968
shivamsinghtomar78 <139133103+shivamsinghtomar78@users.noreply.github.com>
2024-06-23 19:46:56 +0530

    Update reverse linked list-II(leetcode 92).java

--- linked list /reverse linked list-II(leetcode 92).java
+++ linked list /reverse linked list-II(leetcode 92).java
@@ -47,3 +47,40 @@ class Solution {
 
     }
 }
+
+
+
+//Approach-2:
+// t.c :O(n)
+// s.c :O(1)
+
+class Solution {
+    public ListNode reverseBetween(ListNode head, int left, int right) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+        
+        ListNode prev = dummy;
+        
+         
+        for (int i = 1; i < left; i++) {
+            prev = prev.next;
+        }
+        
+        ListNode curr = prev.next;
+        
+         
+        for (int i = 1; i <= right - left; i++) {
+            ListNode temp = prev.next;  
+            prev.next = curr.next;  
+            curr.next = curr.next.next; 
+            prev.next.next = temp;  
+        }
+    
+        return dummy.next;
+    }
+}
+
