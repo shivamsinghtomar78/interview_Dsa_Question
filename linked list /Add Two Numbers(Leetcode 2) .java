677b946b7bf57ece514182605733c62f7183b420
shivamsinghtomar78 <139133103+shivamsinghtomar78@users.noreply.github.com>
2024-06-25 09:26:03 +0530

    Update Add Two Numbers(Leetcode 2) .java

--- linked list /Add Two Numbers(Leetcode 2) .java
+++ linked list /Add Two Numbers(Leetcode 2) .java
@@ -12,7 +12,8 @@ class Solution {
             int y = (l2 != null) ? l2.val : 0;
             int sum = carry + x + y;
             carry = sum / 10;
-            curr.next = new ListNode(sum % 10);
+            ListNode node = new ListNode(sum % 10);
+            curr.next=node;
             curr = curr.next;
             if (l1 != null)
                 l1 = l1.next;
