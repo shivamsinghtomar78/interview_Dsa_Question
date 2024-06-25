2509011e02a1b39238d4177ae00703aac567e754
shivamsinghtomar78 <139133103+shivamsinghtomar78@users.noreply.github.com>
2024-06-25 23:14:10 +0530

    Update Split Linked List in Parts(Leetcode 725).java

--- linked list /Split Linked List in Parts(Leetcode 725).java
+++ linked list /Split Linked List in Parts(Leetcode 725).java
@@ -1,4 +1,4 @@
-//Appraoch -1:splitting list in k part by travelling in whole list
+//Appraoch -1:splitting list in k part by travelling in whole list using arraylist
 //T.c :O(n);
 //S.c :O(k);
 
@@ -40,6 +40,43 @@ class Solution {
         ListNode[] ans = new ListNode[k];
         ans = arr.toArray(ans);
         return ans;
+        
+    }
+}
+
+
+//Approach-2:add in array
+//T.c=O(n);
+// S.c=O(k);
+
+class Solution {
+    public int length(ListNode head) {
+        int n = 0;
+        ListNode temp = head;
+        while (temp != null) {
+            temp = temp.next;
+            n++;
+        }
+        return n;
+    }
+
+    public ListNode[] splitListToParts(ListNode head, int k) {
+        int n=length(head);
+        int size=n/k;
+        int extra=n%k;
+        ListNode []result=new ListNode[k];
+        ListNode curr=head;
+        ListNode prev=null;
+        for(int i=0;i<k;i++){
+            result[i]=curr;
+            for(int j=0;j<size+(extra>0 ? 1:0);j++){
+                prev=curr;
+                curr=curr.next;
+            }
+           if(prev!=null) prev.next=null;
+            extra--;
+        }
+        return result;
         
     }
 }
