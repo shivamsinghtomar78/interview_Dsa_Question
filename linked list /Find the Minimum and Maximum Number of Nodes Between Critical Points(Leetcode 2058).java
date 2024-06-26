52e11b85650c64e8aa53bf2d1adc1edd68f944ad
shivamsinghtomar78 <139133103+shivamsinghtomar78@users.noreply.github.com>
2024-06-26 14:44:28 +0530

    Update Find the Minimum and Maximum Number of Nodes Between Critical Points(Leetcode 2058).java

--- linked list /Find the Minimum and Maximum Number of Nodes Between Critical Points(Leetcode 2058).java
+++ linked list /Find the Minimum and Maximum Number of Nodes Between Critical Points(Leetcode 2058).java
@@ -43,3 +43,42 @@ class Solution {
         return result;
     }
 }
+
+
+// Approach -2:finding max and min 
+// T.c:O(n);
+//S.c:O(n);
+
+class Solution {
+    public int[] nodesBetweenCriticalPoints(ListNode head) {
+        ListNode left=head;
+        ListNode mid=head.next;
+        ListNode right=head.next!=null ?head.next.next:null;
+        int first=-1;
+        int last=-1;
+        int idx=1;
+        int []arr={-1,-1};
+        int minDistance=Integer.MAX_VALUE;
+        while(right!=null){
+            if((mid.val>left.val && mid.val>right.val) || (mid.val<left.val && mid.val<right.val) ){
+                if(first==-1) first=idx;
+                if(last!=-1){
+                    int dist=idx-last;
+                    minDistance=Math.min(minDistance,dist);
+                }
+                last=idx;
+            }
+            idx++;
+            left=left.next;
+            mid=mid.next;
+            right=right.next;
+        }
+        if(first==last) return arr;
+        int maxDistance=last-first;
+        arr[0]=minDistance;
+        arr[1]=maxDistance;
+
+        return arr;
+        
+    }
+}
