bef2363fe8bc678d01d4096583f1e84a39414f3f
shivamsinghtomar78 <139133103+shivamsinghtomar78@users.noreply.github.com>
2024-06-29 18:08:41 +0530

    Update Valid paranthesis(Leetcode 20).java

--- Stack/Valid paranthesis(Leetcode 20).java
+++ Stack/Valid paranthesis(Leetcode 20).java
@@ -23,3 +23,36 @@ class Solution {
         
     }
 }
+
+//Approach -2:iterative method and tricky
+// T.c:O(n);
+// S.c :O(n);
+
+class Solution {
+    public boolean isValid(String s) {
+        int n=s.length();
+        Stack<Character> st=new Stack<>();
+        for(int i=0;i<n;i++){
+            char c=s.charAt(i);
+            if(c=='('){
+                st.push(')');
+            }
+            else if(c=='{'){
+                st.push('}');
+            }
+            else if(c=='['){
+                st.push(']');
+            }
+            else if(st.isEmpty() || st.peek()!=c){
+                return false;
+            }
+            else{
+                st.pop();
+            }
+             
+        }
+        return st.isEmpty();
+        
+    }
+}
+
