4e6adb412bc773c3e3f2065fae1d2007d1291e08
shivamsinghtomar78 <139133103+shivamsinghtomar78@users.noreply.github.com>
2024-07-02 22:46:14 +0530

    Update Three consecutive odds(Leetcode 1550).java

--- Array/Three consecutive odds(Leetcode 1550).java
+++ Array/Three consecutive odds(Leetcode 1550).java
@@ -29,6 +29,19 @@ Approach-2:Counting
 Time complexity :O(n);
 Space complexity:O(1); */
 
+class Solution {
+    public boolean threeConsecutiveOdds(int[] arr) {
+        int n=arr.length;
+        int count=0;
+        for(int i=0;i<n;i++){
+            if(arr[i]%2!=0) count++;
+            else count=0;
+            if(count==3) return true;
+        }
+        return false;
+    }
+}
+
 
 
   
