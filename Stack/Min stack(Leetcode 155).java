66c013449a833c98f858325ae7d35233ace138d4
shivamsinghtomar78 <139133103+shivamsinghtomar78@users.noreply.github.com>
2024-07-03 12:08:21 +0530

    Update Min stack(Leetcode 155).java

--- Stack/Min stack(Leetcode 155).java
+++ Stack/Min stack(Leetcode 155).java
@@ -1,5 +1,5 @@
 /*
-Approach-1:Making new min stack;
+Approach-1:using extra stack;
 Time complexity:O(1);
 Time complexity:O(n); */
 
@@ -34,8 +34,61 @@ class MinStack {
       return  st.peek();
         
     }
+
+
+/*
+Approach-2:using not extra stack;
+Time complexity:O(1);
+Time complexity:O(1); */
+
+
+class MinStack {
+    Stack<Long> st=new Stack<>();
+    long min=-1;
+
+    public MinStack() {
+        
+    }
+    
+    public void push(int  val) {
+        long x=(long)val;
+         if(st.isEmpty()){
+            st.push(x);
+            min=x;
+         }
+         else if(x>=min){
+            st.push(x);
+         }
+         else{
+            st.push(2*x-min);
+            min=x;
+         }
+    }
+    
+    public void pop() {
+         if(st.isEmpty()) return ;
+         else if(st.peek()>=min) st.pop();
+         else if(st.peek()<min){
+            long oldMin=2*min-st.peek();
+            min=oldMin;
+            st.pop();
+         }
+        
+    }
+    
+    public int top() {
+      if(st.isEmpty()) return -1;
+      long q=st.peek();
+      if(q>=min) return (int)(q);
+     if(q<min) return (int)min;
+return 0;
+        
+    }
     
     public int getMin() {
-        return min.peek();
+         if(st.isEmpty()) return -1;
+         return (int)min;
     }
-}
+}    
+    
+   
