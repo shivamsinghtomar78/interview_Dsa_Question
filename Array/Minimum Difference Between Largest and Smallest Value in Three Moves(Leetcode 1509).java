089427a9ea642229b3cfd7c1343ce93eaee84d85
shivamsinghtomar78 <139133103+shivamsinghtomar78@users.noreply.github.com>
2024-07-03 22:23:35 +0530

    Update Minimum Difference Between Largest and Smallest Value in Three Moves(Leetcode 1509).java

--- Array/Minimum Difference Between Largest and Smallest Value in Three Moves(Leetcode 1509).java
+++ Array/Minimum Difference Between Largest and Smallest Value in Three Moves(Leetcode 1509).java
@@ -18,4 +18,25 @@ class Solution {
         return minDiff;
     }
 }
+
+
+/*
+Approach-2:
+Time complexity:O(nlogn)
+Space complexity:O(1) */
+
+class Solution {
+    public int minDifference(int[] nums) {
+        int n=nums.length;
+        if(n<4) return 0;
+        Arrays.sort(nums);
+        int Min=Integer.MAX_VALUE;
+        Min=Math.min(Min,nums[n-4]-nums[0]);
+        Min=Math.min(Min,nums[n-3]-nums[1]);
+        Min=Math.min(Min,nums[n-2]-nums[2]);
+        Min=Math.min(Min,nums[n-1]-nums[3]);
+        return Min;
+        
+    }
+}
   
