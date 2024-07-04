3a668c076a4a11ee6ec6d6c1dd46cbf885cdf0b2
shivamsinghtomar78 <139133103+shivamsinghtomar78@users.noreply.github.com>
2024-07-04 12:01:56 +0530

    Update Merge Nodes in Between Zeros(Leetcode 2181 ).java

--- linked list /Merge Nodes in Between Zeros(Leetcode 2181 ).java
+++ linked list /Merge Nodes in Between Zeros(Leetcode 2181 ).java
@@ -23,3 +23,60 @@ class Solution {
         return newHead.next;
     }
 }
+
+/*
+Approach-2: Recursion 
+Time complexity:O(n);
+Space complexity:O(n); */
+
+
+class Solution {
+    public ListNode mergeNodes(ListNode head) {
+       head=head.next;
+       if(head==null) return head; 
+       ListNode temp=head;
+       int sum=0;
+       while(temp!=null && temp.val!=0){
+        sum+=temp.val;
+        temp=temp.next;
+       }
+       head.val=sum;
+       head.next=mergeNodes(temp);
+       return head;
+    }
+}
+
+/*
+Approach-3: Editorial solution
+Time complexity:O(n);
+Space complexity:O(1); */
+
+
+public class Solution {
+
+    public ListNode mergeNodes(ListNode head) {
+        // Initialize a sentinel/dummy node with the first non-zero value.
+        ListNode modify = head.next;
+        ListNode nextSum = modify;
+
+        while (nextSum != null) {
+            int sum = 0;
+            // Find the sum of all nodes until you encounter a 0.
+            while (nextSum.val != 0) {
+                sum += nextSum.val;
+                nextSum = nextSum.next;
+            }
+
+            // Assign the sum to the current node's value.
+            modify.val = sum;
+            // Move nextSum to the first non-zero value of the next block.
+            nextSum = nextSum.next;
+            // Move modify also to this node.
+            modify.next = nextSum;
+            modify = modify.next;
+        }
+        return head.next;
+    }
+}
+
+
