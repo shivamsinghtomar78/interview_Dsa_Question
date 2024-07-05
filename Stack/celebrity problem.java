5957d1863cbbf88a7507a629887382d9853d760d
shivamsinghtomar78 <139133103+shivamsinghtomar78@users.noreply.github.com>
2024-07-05 17:22:32 +0530

    Update celebrity problem.java

--- Stack/celebrity problem.java
+++ Stack/celebrity problem.java
@@ -28,3 +28,43 @@ Problem:https://www.geeksforgeeks.org/problems/the-celebrity-problem/1  */
         return candidate;
     }
 }
+
+
+
+/*
+Approach-1:iterative 
+Time complexity:O(n);
+Space complexity :O(n);
+Problem:https://www.geeksforgeeks.org/problems/the-celebrity-problem/1  */
+
+class Solution
+{ 
+    //Function to find if there is a celebrity in the party or not.
+    int celebrity(int M[][], int n)
+    {
+    	Stack<Integer> st=new Stack<>();
+        for (int i = 0; i <n ; i++) {
+            st.push(i);
+            
+        }
+        while(st.size()>1){
+            int v1=st.pop();
+            int v2=st.pop();
+            if(M[v1][v2]==0) st.push(v1);
+            else if(M[v2][v1]==0) st.push(v2);
+        }
+        if(st.size()==0) return -1;
+        int potential=st.pop();
+        for (int i = 0; i <n ; i++) {
+            if(M[potential][i]==1)  return -1;
+            
+        }
+        for (int i = 0; i <n ; i++) {
+            if(i==potential) continue;
+            if(M[i][potential]==0)  return -1;
+
+        }
+        return potential;
+    }
+}
+
