e942c1cb5e0e499c06f352845ec0417dd81b82b4
shivamsinghtomar78 <139133103+shivamsinghtomar78@users.noreply.github.com>
2024-07-07 21:59:24 +0530

    Create Water Bottles(Leetcode 1518).java

---
+++ Basic Math/Water Bottles(Leetcode 1518).java
@@ -0,0 +1,21 @@
+/*
+Approach-1:brute force
+Time complexity :O(Log(Numbottles))
+Space complexity:O(1)
+  */
+  class Solution {
+    public int numWaterBottles(int numBottles, int numExchange) {
+        int ans =0;
+        int r=0;
+        int n=numBottles;
+        while((n+r)>=numExchange){
+            int exchange=(n+r)/numExchange;
+            ans+=exchange;
+            r=(n+r)%numExchange;
+            n=exchange;
+        }
+        ans+=numBottles;
+        return ans;
+    }
+}
+
