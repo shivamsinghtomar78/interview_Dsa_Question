5f487e57a2d7817b01e5a301c43b3416a464a78e
shivamsinghtomar78 <139133103+shivamsinghtomar78@users.noreply.github.com>
2024-07-09 23:50:20 +0530

    Update Average Waiting Time(Leetcode 1701 ).java

--- Array/Average Waiting Time(Leetcode 1701 ).java
+++ Array/Average Waiting Time(Leetcode 1701 ).java
@@ -22,3 +22,37 @@ class Solution {
         return sum/n;
     }
 }
+
+
+/*
+Approach-2:iterative method
+Time complexity:O(n);
+Space complexity:O(1);
+*/
+
+
+class Solution {
+    public double averageWaitingTime(int[][] customers) {
+        int n = customers.length;
+
+        double totalWaitTime = 0;
+        int currTime = 0;
+
+        for (int[] customer : customers) {
+            int arrivalTime = customer[0];
+            int cookTime = customer[1];
+
+            if (currTime < arrivalTime) {
+                currTime = arrivalTime;
+            }
+
+            int waitTime = currTime + cookTime - arrivalTime;
+
+            totalWaitTime += waitTime;
+
+            currTime += cookTime;
+        }
+
+        return totalWaitTime / n;
+    }
+}
