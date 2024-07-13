c58a6ab39e619ce27a5891957d13846c1a985828
shivamsinghtomar78 <139133103+shivamsinghtomar78@users.noreply.github.com>
2024-07-13 12:07:11 +0530

    Create Invert Binary Tree(Leetcode 226).java

---
+++ Binary tree/Invert Binary Tree(Leetcode 226).java
@@ -0,0 +1,26 @@
+/*
+Approach-1:Dfs
+Time Complexity (TC):O(n)
+n = number of nodes in the tree
+Space Complexity (SC):O(h)
+h = height of the tree
+Worst case: O(n) for a completely unbalanced tree
+Best case: O(log n) for a perfectly balanced tree  
+  */
+
+class Solution {
+    public TreeNode invertTree(TreeNode root) {
+        invert(root);
+        return root;
+    }
+    public void invert(TreeNode root){
+        if(root==null) return;
+        if(root.left==null && root.right==null) return;
+        TreeNode temp=root.left;
+        root.left=root.right;
+        root.right=temp;
+        invert(root.left);
+        invert(root.right);
+
+    }
+}
