67642c2e203c113f47ad87b0cf5d6c1e496733c9
shivamsinghtomar78 <139133103+shivamsinghtomar78@users.noreply.github.com>
2024-07-17 10:42:21 +0530

    Update Same Tree(Leetcode 100).java

--- Binary tree/Same Tree(Leetcode 100).java
+++ Binary tree/Same Tree(Leetcode 100).java
@@ -1,6 +1,7 @@
-Approach -1:
+/*
+Approach -1:using DFS
 Time complexity:O(n);
-Space complexity:O(n);
+Space complexity:O(n); */
 
 class Solution {
     public boolean isSameTree(TreeNode p, TreeNode q) {
@@ -10,6 +11,51 @@ class Solution {
         if(p.val!=q.val) return false;
         if(isSameTree(p.left,q.left)==false) return false;
         if(isSameTree(p.right,q.right)==false) return false;
+        return true;
+    }
+}
+
+
+//Approach-2 :(Using BFS)
+//Time complexity : O(n)
+//Space complexity : O(n)
+class Solution {
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        if (p == null && q == null) {
+            return true;
+        }
+        if (p == null || q == null) {
+            return false;
+        }
+
+        Queue<TreeNode> q1 = new LinkedList<>();
+        Queue<TreeNode> q2 = new LinkedList<>();
+        q1.add(p);
+        q2.add(q);
+
+        while (!q1.isEmpty() && !q2.isEmpty()) {
+            TreeNode first = q1.poll();
+            TreeNode second = q2.poll();
+
+            if (first.val != second.val) {
+                return false;
+            }
+
+            if (first.left != null && second.left != null) {
+                q1.add(first.left);
+                q2.add(second.left);
+            } else if (first.left != null || second.left != null) {
+                return false;
+            }
+
+            if (first.right != null && second.right != null) {
+                q1.add(first.right);
+                q2.add(second.right);
+            } else if (first.right != null || second.right != null) {
+                return false;
+            }
+        }
+
         return true;
     }
 }
