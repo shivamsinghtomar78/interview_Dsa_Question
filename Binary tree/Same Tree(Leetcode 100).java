a97a43906ca8815be151dd97fc988d40fe62ebd3
shivamsinghtomar78 <139133103+shivamsinghtomar78@users.noreply.github.com>
2024-07-17 09:29:39 +0530

    Create Same Tree(Leetcode 100).java

---
+++ Binary tree/Same Tree(Leetcode 100).java
@@ -0,0 +1,15 @@
+Approach -1:
+Time complexity:O(n);
+Space complexity:O(n);
+
+class Solution {
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        if(p==null && q==null) return true;
+        if(p==null && q!=null) return false;
+        if(p!=null && q==null) return false;
+        if(p.val!=q.val) return false;
+        if(isSameTree(p.left,q.left)==false) return false;
+        if(isSameTree(p.right,q.right)==false) return false;
+        return true;
+    }
+}
