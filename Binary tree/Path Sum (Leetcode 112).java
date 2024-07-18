45cbedb726184a5a675573901d3fb0a55eb4470c
shivamsinghtomar78 <139133103+shivamsinghtomar78@users.noreply.github.com>
2024-07-18 19:06:54 +0530

    Update Path Sum (Leetcode 112).java

--- Binary tree/Path Sum (Leetcode 112).java
+++ Binary tree/Path Sum (Leetcode 112).java
@@ -19,3 +19,29 @@ class Solution {
         return pathSum(root, 0, targetSum);
     }
 }
+
+/*
+Approach-2:DFS
+Time complexity:O(N);
+Space complexity:O(H); */
+
+
+class Solution {
+    boolean inOrder (TreeNode root,int sum,int targetSum){
+        if(root==null) return false;
+        sum+=root.val;
+        // if we reached leaf Node
+        if(root.left==null && root.right==null){
+            if(sum==targetSum) return true;
+            return false;
+        }
+        boolean leftSide=inOrder(root.left,sum,targetSum);
+        boolean rightSide=inOrder(root.right,sum,targetSum);
+        return leftSide || rightSide;
+    }
+    public boolean hasPathSum(TreeNode root, int targetSum) {
+        int sum=0;
+        boolean result=inOrder(root,sum,targetSum);
+        return result;
+    }
+}
