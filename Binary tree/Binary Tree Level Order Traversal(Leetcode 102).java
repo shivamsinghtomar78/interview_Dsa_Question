20cd4add71112f6cf651080b0cf421b876388ea0
shivamsinghtomar78 <139133103+shivamsinghtomar78@users.noreply.github.com>
2024-07-19 23:47:44 +0530

    Update Binary Tree Level Order Traversal(Leetcode 102).java

--- Binary tree/Binary Tree Level Order Traversal(Leetcode 102).java
+++ Binary tree/Binary Tree Level Order Traversal(Leetcode 102).java
@@ -29,3 +29,51 @@ space compplexity:O(N) */
         return 1+Math.max(a,b);
     }
 }
+
+
+/*
+Approach-2:BFS
+Time complexity:O(N)
+space compplexity:O(N) */
+
+
+class Solution {
+    private int levels(TreeNode root){
+        if(root==null) return 0;
+        int a=levels(root.left);
+        int b=levels(root.right);
+        return 1+Math.max(a,b);
+    }
+    public static class  pair{
+        TreeNode node;
+        int level;
+        pair(TreeNode node ,int level){
+            this.node=node;
+            this.level=level;
+        }
+    }
+     private  void BFS(TreeNode root, List<List<Integer>> ans){
+        Queue<pair> q=new LinkedList<>();
+        if(root!=null) q.add( new pair(root,0));
+        while(q.size()>0){
+            pair front=q.remove();
+            TreeNode temp=front.node;
+            int lvl=front.level;
+          ans.get(lvl).add(temp.val);
+            if(temp.left!=null) q.add(new pair(temp.left,lvl+1));
+            if(temp.right!=null) q.add(new pair(temp.right,lvl+1));
+        }
+    }
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        List<List<Integer>> ans=new ArrayList<>();
+        int lvl=levels(root);
+        for(int i=0;i<lvl;i++){
+            List<Integer> arr=new ArrayList<>();
+            ans.add(arr);
+            
+            
+        }
+         BFS(root,ans);
+         return ans;
+    }
+}
