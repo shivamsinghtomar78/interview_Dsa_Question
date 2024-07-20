327a68fa8ce34b2635a31511055f12c5f6737922
shivamsinghtomar78 <139133103+shivamsinghtomar78@users.noreply.github.com>
2024-07-20 08:38:27 +0530

    Update Binary Tree Preorder Traversal(Leetcode 144).java

--- Binary tree/Binary Tree Preorder Traversal(Leetcode 144).java
+++ Binary tree/Binary Tree Preorder Traversal(Leetcode 144).java
@@ -1,5 +1,5 @@
 /*
-Approach-1:Recursive Preorder Traversal
+Approach-1:Recursive Preorder Traversal(DFS)
 Time complexity:O(n);
 space complexity:O(n) */
   
@@ -16,4 +16,26 @@ class Solution {
         preorder(root.right,result);
      }
     
+}
+/*
+Approach-2:iterative Preorder Traversal(BFS)
+Time complexity:O(n);
+space complexity:O(n) */
+
+class Solution {
+    public List<Integer> preorderTraversal(TreeNode root) {
+        List<Integer> ans = new ArrayList<>();
+        Stack<TreeNode> st = new Stack<>();
+        if (root != null)
+            st.push(root);
+        while (st.size()> 0) {
+            TreeNode top = st.pop();
+            ans.add(top.val);
+            if (top.right != null)
+                st.push(top.right);
+            if (top.left != null)
+                st.push(top.left);
+        }
+        return ans;
+    }
 }
