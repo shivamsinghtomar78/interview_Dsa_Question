71add1eb9913d271645fae552fbc72bde0a32396
shivamsinghtomar78 <139133103+shivamsinghtomar78@users.noreply.github.com>
2024-07-20 11:34:27 +0530

    Update Binary Tree postorder Traversal(Leetcode 145).java

--- Binary tree/Binary Tree postorder Traversal(Leetcode 145).java
+++ Binary tree/Binary Tree postorder Traversal(Leetcode 145).java
@@ -1,5 +1,5 @@
 /*
-Approach-1:Recursive postorder Traversal
+Approach-1:Recursive postorder Traversal(DFS)
 Time complexity:O(n);
 space complexity:O(n) */
 
@@ -15,5 +15,32 @@ class Solution {
         postorder(root.left,result);
         postorder(root.right,result);
         result.add(root.val);
+    }
+}
+
+/*
+Approach-2: iterative using one stack(BFS)
+Time complexity:O(n);
+space complexity:O(n) */
+
+class Solution {
+    public List<Integer> postorderTraversal(TreeNode root) {
+        // 4 5 2 6 7 3 1
+        //finding the reverse  preOrder then reverse the reverse preOrder
+        List<Integer> ans = new ArrayList<>();
+        Stack<TreeNode> st = new Stack<>();
+        if (root != null)
+            st.push(root);
+        while (st.size() > 0) {
+            TreeNode top = st.pop();
+            ans.add(top.val);
+            if (top.left != null)
+                st.push(top.left);
+            if (top.right != null)
+                st.push(top.right);
+
+        }
+        Collections.reverse(ans);
+        return ans;
     }
 }
