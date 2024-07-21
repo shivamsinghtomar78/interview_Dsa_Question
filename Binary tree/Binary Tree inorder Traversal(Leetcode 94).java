6baae38a86a9ad6af4083de546e725ec888e1fec
shivamsinghtomar78 <139133103+shivamsinghtomar78@users.noreply.github.com>
2024-07-21 08:56:55 +0530

    Update Binary Tree inorder Traversal(Leetcode 94).java

--- Binary tree/Binary Tree inorder Traversal(Leetcode 94).java
+++ Binary tree/Binary Tree inorder Traversal(Leetcode 94).java
@@ -1,5 +1,5 @@
 /*
-Approach-1:Recursive inorder Traversal
+Approach-1:Recursive inorder Traversal(DFS)
 Time complexity:O(n);
 space complexity:O(n) */
 
@@ -14,5 +14,32 @@ class Solution {
         inorder(root.left,result);
         result.add(root.val);
         inorder(root.right,result);
+    }
+}
+
+/*
+Approach-2:Using Stack(BFS);
+Time complexity:O(n);
+space complexity:O(n) */
+
+
+class Solution {
+    public List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> ans=new ArrayList<>();
+        Stack<TreeNode> st=new Stack<>();
+        TreeNode temp=root;
+        while(true){
+            if(temp!=null){
+                st.push(temp);
+                temp=temp.left;
+            }
+            else{
+                if(st.size()==0) break;
+                TreeNode top=st.pop();
+                ans.add(top.val);
+                temp=top.right;
+            }
+        }
+        return ans;
     }
 }
