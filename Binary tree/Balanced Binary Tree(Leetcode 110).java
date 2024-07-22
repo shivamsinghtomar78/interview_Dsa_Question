a61026b5f6ba241b47f945ae8ad31a30949eb4e9
shivamsinghtomar78 <139133103+shivamsinghtomar78@users.noreply.github.com>
2024-07-22 09:12:46 +0530

    Update Balanced Binary Tree(Leetcode 110).java

--- Binary tree/Balanced Binary Tree(Leetcode 110).java
+++ Binary tree/Balanced Binary Tree(Leetcode 110).java
@@ -29,22 +29,21 @@ Time complexity:O(N);
 Space complexity:O(N); */
 
 class Solution {
-    static boolean ans;
     public int levels(TreeNode root){
         if(root==null) return 0;
         int a=levels(root.left);
         int b=levels(root.right);
         int diff=Math.abs(a-b);
-        if(diff>1) ans=false;
+        if(diff>1) ans[0]=false;
         return 1+Math.max(a,b);
     }
     // conditions for balanced treee:
     // |levelOfLeft-levelOfRight|<=1
     
     public boolean isBalanced(TreeNode root) {
-       ans=true;
-       levels(root);
-       return ans;
+       boolean[] ans={true};
+       levels(root,ans);
+       return ans[0];
        
 
     }
