38473392e14635e55fb6e659ba490c573f06fb35
shivamsinghtomar78 <139133103+shivamsinghtomar78@users.noreply.github.com>
2024-07-22 08:46:29 +0530

    Update Balanced Binary Tree(Leetcode 110).java

--- Binary tree/Balanced Binary Tree(Leetcode 110).java
+++ Binary tree/Balanced Binary Tree(Leetcode 110).java
@@ -21,3 +21,31 @@ class Solution {
 
     }
 }
+
+
+/*
+Approach -2:DFS(Optimal)
+Time complexity:O(N);
+Space complexity:O(N); */
+
+class Solution {
+    static boolean ans;
+    public int levels(TreeNode root){
+        if(root==null) return 0;
+        int a=levels(root.left);
+        int b=levels(root.right);
+        int diff=Math.abs(a-b);
+        if(diff>1) ans=false;
+        return 1+Math.max(a,b);
+    }
+    // conditions for balanced treee:
+    // |levelOfLeft-levelOfRight|<=1
+    
+    public boolean isBalanced(TreeNode root) {
+       ans=true;
+       levels(root);
+       return ans;
+       
+
+    }
+}
