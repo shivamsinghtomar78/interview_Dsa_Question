7e888b20c203616a47ad10755f6a0985cd154a05
shivamsinghtomar78 <139133103+shivamsinghtomar78@users.noreply.github.com>
2024-07-26 23:02:40 +0530

    Update Insert into a Binary Search Tree(Leetcode 701).java

--- Binary Search Tree/Insert into a Binary Search Tree(Leetcode 701).java
+++ Binary Search Tree/Insert into a Binary Search Tree(Leetcode 701).java
@@ -16,3 +16,27 @@ Space complexity:O(Log(N)) */
         return root;
     }
 }
+
+
+/*
+Approach-1:Recursion
+Time complexity:O(H)
+Space complexity:O(H) */
+
+ class Solution {
+    public void insert(TreeNode root,int val){
+        if(val<root.val){
+            if(root.left==null) root.left=new TreeNode(val);
+            else insert(root.left,val);
+        }
+         if(val>root.val){
+            if(root.right==null) root.right=new TreeNode(val);
+            else insert(root.right,val);
+        }
+    }
+    public TreeNode insertIntoBST(TreeNode root, int val) {
+       if(root==null) return new TreeNode(val);
+       insert(root,val);
+       return root; 
+    }
+}
