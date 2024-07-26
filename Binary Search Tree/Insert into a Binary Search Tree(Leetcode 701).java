4325c39890fe6463277701762a99052d00b491fc
shivamsinghtomar78 <139133103+shivamsinghtomar78@users.noreply.github.com>
2024-07-26 22:51:13 +0530

    Create  Insert into a Binary Search Tree(Leetcode 701).java

---
+++ Binary Search Tree/Insert into a Binary Search Tree(Leetcode 701).java
@@ -0,0 +1,18 @@
+/*
+Approach-1:Recursion
+Time complexity:O(Log(N))
+Space complexity:O(Log(N)) */
+
+  class Solution {
+    public TreeNode insertIntoBST(TreeNode root, int val) {
+        if (root == null) {
+            return new TreeNode(val);
+        }
+        if (root.val < val) {
+            root.right = insertIntoBST(root.right, val);
+        } else {
+            root.left = insertIntoBST(root.left, val);
+        }
+        return root;
+    }
+}
