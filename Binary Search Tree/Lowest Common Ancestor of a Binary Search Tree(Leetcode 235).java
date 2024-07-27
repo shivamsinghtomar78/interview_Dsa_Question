a5cc1725cae92a070a9b54185180cbf2d0be65c8
shivamsinghtomar78 <139133103+shivamsinghtomar78@users.noreply.github.com>
2024-07-27 18:32:22 +0530

    Create Lowest Common Ancestor of a Binary Search Tree(Leetcode 235).java

---
+++ Binary Search Tree/Lowest Common Ancestor of a Binary Search Tree(Leetcode 235).java
@@ -0,0 +1,28 @@
+/*
+Approach-1:DFS
+Time complexity:O(n);
+Space complexity:O(n);
+*/
+
+class Solution {
+    public boolean exists(TreeNode root, TreeNode node) {
+        if (node == root)
+            return true;
+        if (root == null)
+            return false;
+        return exists(root.left, node) || exists(root.right, node);
+    }
+
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if (p == root || q == root)
+            return root;
+        boolean pLieInLST = exists(root.left, p);
+        boolean qLieInLST = exists(root.left, q);
+        if (pLieInLST == true && qLieInLST == true)
+            return lowestCommonAncestor(root.left, p, q);
+        if (pLieInLST == false && qLieInLST == false)
+            return lowestCommonAncestor(root.right, p, q);
+        else
+            return root;
+    }
+}
