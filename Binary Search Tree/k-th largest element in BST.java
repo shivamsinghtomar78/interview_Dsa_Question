48881b48c69e73a5e00d1d7ebc7006f5e2e8a0a4
shivamsinghtomar78 <139133103+shivamsinghtomar78@users.noreply.github.com>
2024-07-27 09:52:50 +0530

    Update and rename k-th smallest element in BST.java to k-th largest element in BST.java

--- Binary Search Tree/k-th largest element in BST.java
+++ Binary Search Tree/k-th largest element in BST.java
@@ -7,10 +7,10 @@ problem in gfg:https://www.geeksforgeeks.org/problems/kth-largest-element-in-bst
 
 
 class Solution {
-    // Return the Kth smallest element in the given BST
-    public int KthSmallestElement(Node root, int k) {
+    // Return the Kth largest element in the given BST
+    public int kthLargest(Node root, int k) {
         ArrayList<Integer> arr = new ArrayList<>();
-        inorder(root, arr);
+        reverseInorder(root, arr);
         
         if (k > 0 && k <= arr.size()) {
             return arr.get(k - 1);
@@ -18,10 +18,10 @@ class Solution {
         return -1;  // Return -1 if k is out of bounds
     }
     
-    private void inorder(Node root, List<Integer> arr) {
+    private void reverseInorder(Node root, List<Integer> arr) {
         if (root == null) return;
-        inorder(root.left, arr);
+        reverseInorder(root.right, arr);
         arr.add(root.data);
-        inorder(root.right, arr);
+        reverseInorder(root.left, arr);
     }
 }
