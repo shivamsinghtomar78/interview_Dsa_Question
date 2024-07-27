2ce051addaf9036de22e318e160e4952a752b08a
shivamsinghtomar78 <139133103+shivamsinghtomar78@users.noreply.github.com>
2024-07-27 09:49:44 +0530

    Update Kth Smallest Element in a BST(Leetcode 230).java

--- Binary Search Tree/Kth Smallest Element in a BST(Leetcode 230).java
+++ Binary Search Tree/Kth Smallest Element in a BST(Leetcode 230).java
@@ -1,13 +1,18 @@
 /*
 Approach-1:DFS 
 Time complexity:O(N);
-Space complexity:O(N); */
+Space complexity:O(N);
+problem in gfg:https://www.geeksforgeeks.org/problems/find-k-th-smallest-element-in-bst/1
+*/
 
 class Solution {
     public int kthSmallest(TreeNode root, int k) {
     ArrayList<Integer> arr=new ArrayList();
      inorder(root,k,arr);
-      return arr.get(k-1);  
+       if (k > 0 && k <= arr.size()) {
+            return arr.get(k - 1);
+        }
+        return -1
     }
     public void inorder(TreeNode root,int k,List<Integer> arr){
          if(root==null) return;
