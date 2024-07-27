6df9e8b758c724422cdc3683ee9d749a11fc9cd0
shivamsinghtomar78 <139133103+shivamsinghtomar78@users.noreply.github.com>
2024-07-27 22:51:47 +0530

    Create Convert BST to Greater Tree(Leetcode 538).java

---
+++ Binary Search Tree/Convert BST to Greater Tree(Leetcode 538).java
@@ -0,0 +1,24 @@
+/*
+Approach-1:DFS
+Time complexity:O();
+Space complexity:O();
+*/
+
+class Solution {
+    public TreeNode bstToGst(TreeNode root) {
+        List<TreeNode> arr=new ArrayList<>();
+        inorder(root,arr);
+        int n=arr.size();
+        for(int i=n-2;i>=0;i--){
+            arr.get(i).val+=arr.get(i+1).val;
+        }
+        return root;
+
+    }
+    public void inorder(TreeNode root,List<TreeNode> arr){
+        if(root==null) return ;
+        inorder(root.left,arr);
+        arr.add(root);
+        inorder(root.right,arr);
+    }
+}
