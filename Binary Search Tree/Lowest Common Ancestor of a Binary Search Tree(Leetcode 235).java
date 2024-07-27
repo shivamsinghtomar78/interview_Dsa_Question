5771c21b4df5441e9feb80b4480bfe288e29a58e
shivamsinghtomar78 <139133103+shivamsinghtomar78@users.noreply.github.com>
2024-07-27 19:01:19 +0530

    Update Lowest Common Ancestor of a Binary Search Tree(Leetcode 235).java

--- Binary Search Tree/Lowest Common Ancestor of a Binary Search Tree(Leetcode 235).java
+++ Binary Search Tree/Lowest Common Ancestor of a Binary Search Tree(Leetcode 235).java
@@ -26,3 +26,19 @@ class Solution {
             return root;
     }
 }
+
+
+/*
+Approach-2:DFS
+Time complexity:O(H);
+Space complexity:O(H);
+*/
+
+
+class Solution {
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+       if(root.val<p.val && root.val<q.val) return  lowestCommonAncestor(root.right, p,q);
+      else if(root.val>p.val && root.val>q.val) return lowestCommonAncestor(root.left, p,q);
+      else return root;
+    }
+}
