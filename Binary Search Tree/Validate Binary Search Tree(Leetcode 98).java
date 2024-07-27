77033d2437e17cb29bfb84560212dabeeaf51945
shivamsinghtomar78 <139133103+shivamsinghtomar78@users.noreply.github.com>
2024-07-27 11:57:34 +0530

    Update Validate Binary Search Tree(Leetcode 98).java

--- Binary Search Tree/Validate Binary Search Tree(Leetcode 98).java
+++ Binary Search Tree/Validate Binary Search Tree(Leetcode 98).java
@@ -1,5 +1,5 @@
 /*
-Approach-1:
+Approach-1:DFS
 Time complexity:O(N);
 Space complexity:O(N);  */
 
@@ -23,6 +23,28 @@ class Solution {
         long b=min(root.left);
         long c=min(root.right);
         return Math.min(a,Math.min(b,c));
+    }
+}
+
+/*
+Approach-1:DFS
+Time complexity:O(N);
+Space complexity:O(N);  */
+
+class Solution {
+    public boolean isValidBST(TreeNode root) {
+        List<Integer> arr=new ArrayList();
+        inorder(root,arr);
+        for(int i=0;i<arr.size()-1;i++){
+            if(arr.get(i)>=arr.get(i+1)) return false;
+        }
+        return true;
+    }
+    public void inorder(TreeNode root, List<Integer> arr){
+        if(root==null) return ;
+        inorder(root.left,arr);
+        arr.add(root.val);
+        inorder(root.right,arr);
     }
 }
 
