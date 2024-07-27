2a09976b7706ce589a87e7c97fe990adee66475c
shivamsinghtomar78 <139133103+shivamsinghtomar78@users.noreply.github.com>
2024-07-27 23:07:47 +0530

    Update Binary Search Tree to Greater Sum Tree(Leetcode1038).java

--- Binary Search Tree/Binary Search Tree to Greater Sum Tree(Leetcode1038).java
+++ Binary Search Tree/Binary Search Tree to Greater Sum Tree(Leetcode1038).java
@@ -22,3 +22,26 @@ class Solution {
         inorder(root.right,arr);
     }
 }
+
+/*
+Approach-2:DFS
+Time Complexity: O(n)
+Space Complexity: O(n)
+*/
+
+class Solution {
+    static int sum;
+    public TreeNode bstToGst(TreeNode root) {
+        sum=0;
+        reverseInorder(root);
+        return root;
+    }
+    public void reverseInorder(TreeNode root){
+        if(root==null) return;
+        reverseInorder(root.right);
+        root.val+=sum;
+        sum=root.val;
+        reverseInorder(root.left);
+    }
+
+}
