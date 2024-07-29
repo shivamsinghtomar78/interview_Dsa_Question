008140fb2a39048c71a8c0cf36f1c23ce1b519da
shivamsinghtomar78 <139133103+shivamsinghtomar78@users.noreply.github.com>
2024-07-29 12:08:31 +0530

    Update Binary Tree inorder Traversal(Leetcode 94).java

--- Binary tree/Binary Tree inorder Traversal(Leetcode 94).java
+++ Binary tree/Binary Tree inorder Traversal(Leetcode 94).java
@@ -43,3 +43,40 @@ class Solution {
         return ans;
     }
 }
+
+
+/*
+Approach-3:Using Morris Traversal;
+Time complexity:O(n);
+space complexity:O(1) */
+
+
+class Solution {
+    public List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> ans=new ArrayList<>();
+        TreeNode curr=root;
+        while(curr!=null){
+            if(curr.left!=null){
+                TreeNode pred=curr.left;
+                while(pred.right!=null && pred.right!=curr) pred=pred.right;
+                
+                if(pred.right==null){//link
+                pred.right=curr;
+                curr=curr.left;
+                }
+                else{//pred.right=curr:unlink
+                pred.right=null;
+                ans.add(curr.val);
+                curr=curr.right;
+
+                }
+            }
+            else{
+                ans.add(curr.val);
+                curr=curr.right;
+            }
+        }
+        return ans;
+    }
+}
+
