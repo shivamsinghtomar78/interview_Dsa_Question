1144181b6c67b6df540b8073516d9b1d7d904987
shivamsinghtomar78 <139133103+shivamsinghtomar78@users.noreply.github.com>
2024-07-29 23:30:58 +0530

    Update Flatten Binary Tree to Linked List(Leetcode 114).java

--- Binary Search Tree/Flatten Binary Tree to Linked List(Leetcode 114).java
+++ Binary Search Tree/Flatten Binary Tree to Linked List(Leetcode 114).java
@@ -25,3 +25,27 @@ class Solution {
         preorder(root.right, nodes);
     }
 }
+/*
+Approach-2 :purely recursion
+Time complexity:O(N);
+Space complexity:O(H);
+*/
+ class Solution {
+    private TreeNode prev = null;
+
+    public void flatten(TreeNode root) {
+        if (root == null) return;
+
+        // Recursively flatten the right subtree
+        flatten(root.right);
+        // Recursively flatten the left subtree
+        flatten(root.left);
+
+        // Set the right pointer to the previously processed node
+        root.right = prev;
+        // Set the left pointer to null
+        root.left = null;
+        // Update the prev pointer to the current node
+        prev = root;
+    }
+}
