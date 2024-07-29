65fbbed8614f124d5fbb53ff304c8ebfe2f9065c
shivamsinghtomar78 <139133103+shivamsinghtomar78@users.noreply.github.com>
2024-07-29 12:09:39 +0530

    Create BST in order Traversal.java

---
+++ Binary Search Tree/BST in order Traversal.java
@@ -0,0 +1,81 @@
+/*
+Approach-1:Recursive inorder Traversal(DFS)
+Time complexity:O(n);
+space complexity:O(n) */
+
+class Solution {
+    public List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> result=new ArrayList<>();
+        inorder(root,result);
+        return result;
+    }
+    public void inorder(TreeNode root,List<Integer>  result){
+        if(root==null) return ;
+        inorder(root.left,result);
+        result.add(root.val);
+        inorder(root.right,result);
+    }
+}
+
+/*
+Approach-2:Using Stack(BFS);
+Time complexity:O(n);
+space complexity:O(n) */
+
+
+class Solution {
+    public List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> ans=new ArrayList<>();
+        Stack<TreeNode> st=new Stack<>();
+        TreeNode temp=root;
+        while(true){
+            if(temp!=null){
+                st.push(temp);
+                temp=temp.left;
+            }
+            else{
+                if(st.size()==0) break;
+                TreeNode top=st.pop();
+                ans.add(top.val);
+                temp=top.right;
+            }
+        }
+        return ans;
+    }
+}
+
+
+/*
+Approach-3:Using Morris Traversal;
+Time complexity:O(n);
+space complexity:O(1) */
+
+
+class Solution {
+    public List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> ans=new ArrayList<>();
+        TreeNode curr=root;
+        while(curr!=null){
+            if(curr.left!=null){
+                TreeNode pred=curr.left;
+                while(pred.right!=null && pred.right!=curr) pred=pred.right;
+                
+                if(pred.right==null){//link
+                pred.right=curr;
+                curr=curr.left;
+                }
+                else{//pred.right=curr:unlink
+                pred.right=null;
+                ans.add(curr.val);
+                curr=curr.right;
+
+                }
+            }
+            else{
+                ans.add(curr.val);
+                curr=curr.right;
+            }
+        }
+        return ans;
+    }
+}
