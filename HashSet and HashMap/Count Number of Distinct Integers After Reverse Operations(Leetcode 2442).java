6e7e16070628ea87594ccb68271239ad48f4af8b
shivamsinghtomar78 <139133103+shivamsinghtomar78@users.noreply.github.com>
2024-07-30 18:00:55 +0530

    Update Count Number of Distinct Integers After Reverse Operations(Leetcode 2442).java

--- HashSet and HashMap/Count Number of Distinct Integers After Reverse Operations(Leetcode 2442).java
+++ HashSet and HashMap/Count Number of Distinct Integers After Reverse Operations(Leetcode 2442).java
@@ -1,5 +1,5 @@
 /*
-Approach-1:HashSet:
+Approach-1:HashSet and using ArrayList;
 Time complexity:O(N);
 Space complexity:O(N); */
 
@@ -22,6 +22,29 @@ class Solution {
         HashSet<Integer> set=new HashSet<>();
         for(int ele:arr){
             set.add(ele);
+        }
+        return set.size();
+    }
+}
+
+/*
+Approach-2:HashSet ;
+Time complexity:O(N);
+Space complexity:O(N); */
+
+
+class Solution {
+    public int countDistinctIntegers(int[] nums) {
+        HashSet<Integer> set=new HashSet<>();
+        for(int ele:nums){
+            set.add(ele);
+            int reverse=0;
+            while(ele!=0){
+                int digit=ele%10;
+                reverse=reverse*10+digit;
+                ele=ele/10;
+            }
+            set.add(reverse);
         }
         return set.size();
     }
