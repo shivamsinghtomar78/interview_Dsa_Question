645a123702e9212aab49f866e8e5ee425398bc06
shivamsinghtomar78 <139133103+shivamsinghtomar78@users.noreply.github.com>
2024-07-30 11:36:40 +0530

    Update Flatten Binary Tree to Linked List(Leetcode 114).java

--- Binary Search Tree/Flatten Binary Tree to Linked List(Leetcode 114).java
+++ Binary Search Tree/Flatten Binary Tree to Linked List(Leetcode 114).java
@@ -49,3 +49,30 @@ Space complexity:O(H);
         prev = root;
     }
 }
+
+/*
+Approach-3 : Using morris Traversal
+Time complexity:O(N);
+Space complexity:O(1);
+*/
+
+class Solution {
+    public void flatten(TreeNode root) {
+        TreeNode curr=root;
+        while(curr!=null){
+            if(curr.left!=null){
+                TreeNode r=curr.right;
+                curr.right=curr.left;
+                TreeNode pred=curr.left;
+                while(pred.right!=null) pred=pred.right;
+                pred.right=r;
+                curr.left=null;
+                curr=curr.right;
+            }
+            else{
+                curr=curr.right;
+
+            }
+        }
+    }
+}
