d7f89f0acde05d29999217a49be89d39bd584f40
shivamsinghtomar78 <139133103+shivamsinghtomar78@users.noreply.github.com>
2024-07-30 19:29:11 +0530

    Update Find Maximum Number of String Pairs(Leetcode 2744).java

--- HashSet and HashMap/Find Maximum Number of String Pairs(Leetcode 2744).java
+++ HashSet and HashMap/Find Maximum Number of String Pairs(Leetcode 2744).java
@@ -17,3 +17,30 @@ class Solution {
         return count;
     }
 }
+
+
+/*
+Approach-2:Brute force ;
+Time complexity:O(N);
+Space complexity:O(N);
+*/
+
+class Solution {
+    public int maximumNumberOfStringPairs(String[] words) {
+        HashSet<String> set=new HashSet<>();
+        int count=0;
+        for(int i=0;i<words.length;i++){
+            String rev=reverse(words[i]);
+            if(set.contains(rev)){
+                count++;
+            }
+            else set.add(words[i]);
+        } 
+        return count ;
+    }
+    public String reverse(String s){
+        StringBuilder sb=new StringBuilder(s);
+        sb.reverse();
+        return sb.toString();
+    }
+}
