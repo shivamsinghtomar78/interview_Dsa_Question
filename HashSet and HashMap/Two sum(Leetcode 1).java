1df6fc0cb8a73e217d3eb5d166acc1828a0a1ec8
shivamsinghtomar78 <139133103+shivamsinghtomar78@users.noreply.github.com>
2024-07-31 07:18:30 +0530

    Create Two sum(Leetcode 1).java

---
+++ HashSet and HashMap/Two sum(Leetcode 1).java
@@ -0,0 +1,18 @@
+/*
+Approach-1:HashMap
+Time complexity:O(N);
+Space complexity:O(N); */
+
+
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> numToIndex = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            if (numToIndex.containsKey(target - nums[i])) {
+                return new int[] {numToIndex.get(target - nums[i]), i};
+            }
+            numToIndex.put(nums[i], i);
+        }
+        return new int[] {};
+    }
+}
