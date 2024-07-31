229fa448a095dec85bc5285547fdbbe685a8e16a
shivamsinghtomar78 <139133103+shivamsinghtomar78@users.noreply.github.com>
2024-07-31 07:32:45 +0530

    Update Two sum(Leetcode 1).java

--- HashSet and HashMap/Two sum(Leetcode 1).java
+++ HashSet and HashMap/Two sum(Leetcode 1).java
@@ -16,3 +16,22 @@ class Solution {
         return new int[] {};
     }
 }
+
+
+/*
+Approach-1:HashMap
+Time complexity:O(N^2);
+Space complexity:O(1); */
+
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        int n=nums.length;
+        for(int i=0;i<n;i++){
+            for(int j=i+1;j<n;j++){
+                if(nums[i]+nums[j]==target) return new int[] {i,j};
+            }
+        }
+        return new int[] {};
+    }
+}
+
