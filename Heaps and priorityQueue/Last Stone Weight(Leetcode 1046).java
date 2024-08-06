7d54b194256ba72e4765ab63707b8eac101de92c
shivamsinghtomar78 <139133103+shivamsinghtomar78@users.noreply.github.com>
2024-08-06 09:35:03 +0530

    Update Last Stone Weight(Leetcode 1046).java

--- Heaps and priorityQueue/Last Stone Weight(Leetcode 1046).java
+++ Heaps and priorityQueue/Last Stone Weight(Leetcode 1046).java
@@ -25,4 +25,26 @@ class Solution {
         if(pq.size()==0) return 0;
         return pq.remove();
     }
+
+
+    /*
+Approach-2:ArrayList;
+Time complexity:O(N^2logN);
+Space complexity:O(N); */
+
+ class Solution {
+   public int lastStoneWeight(int[] stones) {
+        List<Integer> arr=new ArrayList<>();
+        for(int ele:stones){
+            arr.add(ele);
+        }
+        while(arr.size()>1){
+            Collections.sort(arr);
+            int y=arr.remove(arr.size()-1);
+            int x=arr.remove(arr.size()-1);
+            if(y!=x) arr.add(y-x);
+        }
+        if(arr.size()==0) return 0;
+        else return arr.get(0);
+    }
 }
