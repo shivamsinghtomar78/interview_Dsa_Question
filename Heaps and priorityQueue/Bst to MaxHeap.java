74bf02d383650c8afcfa96fc0c41d302326e56f6
shivamsinghtomar78 <139133103+shivamsinghtomar78@users.noreply.github.com>
2024-08-07 23:07:39 +0530

    Create Bst to MaxHeap.java

---
+++ Heaps and priorityQueue/Bst to MaxHeap.java
@@ -0,0 +1,31 @@
+/*
+Appraoch-1:using inorder and preorder traversal in reverse;
+Time complexity:O(N^2);
+Space complexity:O(N);
+*/
+
+class Solution
+{
+    public static void convertToMaxHeapUtil(Node root)
+    {
+       List<Integer> helper=new ArrayList<>();
+       inorder(root,helper);
+       preorder(root,helper);
+       
+    }
+    private static  void inorder(Node root, List<Integer> helper){
+        if(root==null) return;
+        inorder(root.right,helper);
+        helper.add(root.data);
+        inorder(root.left,helper);
+      
+    }
+   private static void preorder(Node root,List<Integer> helper){
+        if(root==null) return;
+        root.data=helper.remove(0);
+        preorder(root.right,helper);
+        preorder(root.left,helper);
+         
+    
+    }
+}
