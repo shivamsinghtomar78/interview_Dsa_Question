d0573dc314f3a8107e22fb01bf246d5d5c0c0505
shivamsinghtomar78 <139133103+shivamsinghtomar78@users.noreply.github.com>
2024-08-08 23:40:46 +0530

    Update Running sum of 1D Array(Leetcode 1480).java

--- Prefix Sum/Running sum of 1D Array(Leetcode 1480).java
+++ Prefix Sum/Running sum of 1D Array(Leetcode 1480).java
@@ -15,3 +15,20 @@ class Solution {
         return pre;
     }
 }
+
+/*
+Approach-2:Prefix Sum;
+Time complexity:O(n);
+Space complexity:O(1);
+*/
+
+class Solution {
+    public int[] runningSum(int[] arr) {
+        int n=arr.length;
+        for(int i=1;i<n;i++){
+            arr[i]+=arr[i-1];
+        }
+        return arr;
+         
+    }
+}
