90287a902a5938a05485575b16d2002272ddcced
shivamsinghtomar78 <139133103+shivamsinghtomar78@users.noreply.github.com>
2024-08-09 09:47:20 +0530

    Update Range sum query Immutable(Leetcode 303).java

--- Prefix Sum/Range sum query Immutable(Leetcode 303).java
+++ Prefix Sum/Range sum query Immutable(Leetcode 303).java
@@ -1,6 +1,6 @@
 /*
 Approach-1:Prefix Sum;
-Time complexity:O(n);
+Time complexity:sumRange:O(n),constructor:O(1);
 Space complexity:O(n);
 */
 
@@ -18,3 +18,25 @@ class NumArray {
         return sum;
     }
 }
+
+/*
+Approach-2:Prefix Sum;
+Time complexity:sumRange:O(1),constructor:O(n);
+Space complexity:O(n);
+*/
+
+class NumArray {
+    int [] arr;
+    public NumArray(int[] nums) {
+        arr=Arrays.copyOf(nums,nums.length);
+        for(int i=1;i<nums.length;i++){
+            arr[i]+=arr[i-1];
+        }
+    }
+    
+    public int sumRange(int left, int right) {
+        if(left==0) return arr[right];
+        return arr[right]-arr[left-1];
+    }
+}
+
