1852c0ecce9c1acfa588b14426030f2ff9d23148
shivamsinghtomar78 <139133103+shivamsinghtomar78@users.noreply.github.com>
2024-08-10 23:28:33 +0530

    Update Corporate Flight Bookings(Leetcode 1109).java

--- Prefix Sum/Corporate Flight Bookings(Leetcode 1109).java
+++ Prefix Sum/Corporate Flight Bookings(Leetcode 1109).java
@@ -17,5 +17,29 @@ class Solution {
         return ans;
     }
 }
+
+/*
+Approach-1:Prefix Sum;
+Time complexity:O(n+m);
+Space complexity:O(n);
+*/
+
+
+class Solution {
+    public int[] corpFlightBookings(int[][] arr, int n) {
+        int[]ans=new int[n];
+        for(int i=0;i<arr.length;i++){
+            int first=arr[i][0];
+            int last=arr[i][1];
+            int seats=arr[i][2];
+            ans[first-1]+=seats;
+          if(last<n)  ans[last]-=seats;
+        }
+        for(int i=1;i<n;i++){
+            ans[i]+=ans[i-1];
+        }
+        return ans;
+    }
+}
   
 
