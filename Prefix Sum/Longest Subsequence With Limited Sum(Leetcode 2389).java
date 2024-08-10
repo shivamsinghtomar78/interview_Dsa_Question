a3be9c13fc6ff723b34089ac49da616cf180ae4f
shivamsinghtomar78 <139133103+shivamsinghtomar78@users.noreply.github.com>
2024-08-10 20:19:27 +0530

    Update Longest Subsequence With Limited Sum(Leetcode 2389).java

--- Prefix Sum/Longest Subsequence With Limited Sum(Leetcode 2389).java
+++ Prefix Sum/Longest Subsequence With Limited Sum(Leetcode 2389).java
@@ -1,6 +1,6 @@
 /*
 Approach-1:Prefix Sum;
-Time complexity:O(nlogn+n*m);
+Time complexity:O((m+n)logn);
 Space complexity:O(n + m);
 */
 
@@ -26,3 +26,35 @@ class Solution {
         return ans;
     }
 }
+
+/*
+Approach-1:Prefix Sum and binary search;
+Time complexity:O((m+n)logn);
+Space complexity:O(n + m);
+*/
+
+class Solution {
+    public int[] answerQueries(int[] nums, int[] queries) {
+        int n=nums.length;
+        int m=queries.length;
+        Arrays.sort(nums);
+        for(int i=1;i<n;i++){
+            nums[i]+=nums[i-1];
+        }
+        int[]ans=new int[m];
+        for(int i=0;i<m;i++){
+             int lo=0;
+             int hi=n-1;
+             while(lo<=hi){
+                int mid=lo+(hi-lo)/2;
+                if(nums[mid]>queries[i]) hi=mid-1;
+                else{
+                    ans[i]=Math.max(ans[i],mid+1);
+                    lo=mid+1;
+                }
+             }
+        }
+        return ans;
+    }
+}
+
