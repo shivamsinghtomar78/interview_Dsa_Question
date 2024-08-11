1e1ee229cd65b120547a1c18ac00d3da08679734
shivamsinghtomar78 <139133103+shivamsinghtomar78@users.noreply.github.com>
2024-08-11 11:09:27 +0530

    Update Subarray Sum Equals K(Leetcode 560).java

--- Prefix Sum/Subarray Sum Equals K(Leetcode 560).java
+++ Prefix Sum/Subarray Sum Equals K(Leetcode 560).java
@@ -38,3 +38,32 @@ public class Solution {
         return count;
     }
 }
+
+/*
+Approach-3:HashMap and prefix sum;
+Time complexity:O(n);
+Space complexity:O(n);
+*/
+
+public class Solution {
+    public int subarraySum(int[] arr, int k) {
+         int n=arr.length;
+         int count=0;
+         for(int i=1;i<n;i++){
+            arr[i]+=arr[i-1];
+         }
+         Map<Integer,Integer> map=new HashMap<>();
+         for(int i=0;i<n;i++){
+            int ele=arr[i];
+            if(ele==k) count++;
+            int rem=ele-k;
+            if(map.containsKey(rem)) count+=map.get(rem);
+            if(map.containsKey(ele)){
+                int freq=map.get(ele);
+                map.put(ele,freq+1);
+            }
+            else map.put(ele,1);
+         }
+         return count;
+    }
+}
