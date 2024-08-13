064e18140940c275444723ed78b44fb0a09357f7
shivamsinghtomar78 <139133103+shivamsinghtomar78@users.noreply.github.com>
2024-08-13 22:05:09 +0530

    Create Max Consecutive Ones III(Leetcode 1004).java

---
+++ Sliding Window  /Max Consecutive Ones III(Leetcode 1004).java
@@ -0,0 +1,20 @@
+/*
+Approach-1:sliding window
+Time Complexity: O(N)
+Space Complexity: O(1) */
+class Solution {
+    public int longestOnes(int[] arr, int k) {
+        int n=arr.length,i=0,zeroes=0,max=0;
+        for(int j=0;j<n;j++){
+            if(arr[j]==0) zeroes++;
+            while(zeroes>k){
+                if(arr[i]==0) zeroes--;
+                i++;
+            }
+            max=Math.max(max,j-i+1);
+
+        }
+        return max;
+
+    }
+}
