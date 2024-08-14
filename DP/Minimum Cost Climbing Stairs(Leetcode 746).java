621fa2a54edc29841daf77eb07d5cd9a7d8f6464
shivamsinghtomar78 <139133103+shivamsinghtomar78@users.noreply.github.com>
2024-08-14 23:54:31 +0530

    Update Minimum Cost Climbing Stairs(Leetcode 746).java

--- DP/Minimum Cost Climbing Stairs(Leetcode 746).java
+++ DP/Minimum Cost Climbing Stairs(Leetcode 746).java
@@ -17,3 +17,22 @@ class Solution {
         return Math.min(minCost(cost,n-1,dp),minCost(cost,n-2,dp));
     }
 }
+
+/*
+Approach-2:Bottom-up dp or iterative dp or tabulation dp;
+Time complexity:O(n);
+Space complexity:O(n);
+*/
+
+class Solution {
+    public int minCostClimbingStairs(int[] cost) {
+        int n=cost.length;
+        int []dp=new int[n];
+        dp[0]=cost[0];
+        dp[1]=cost[1];
+        for(int i=2;i<n;i++){
+            dp[i]=cost[i]+Math.min(dp[i-1],dp[i-2]);
+        }
+        return Math.min(dp[n-1],dp[n-2]);
+    }
+}
