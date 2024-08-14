c294a112ad254490b0af9359515c719d7ca1149a
shivamsinghtomar78 <139133103+shivamsinghtomar78@users.noreply.github.com>
2024-08-14 22:08:40 +0530

    Update Fibonacci Number (Leetcode 509)).java

--- DP/Fibonacci Number (Leetcode 509)).java
+++ DP/Fibonacci Number (Leetcode 509)).java
@@ -14,3 +14,22 @@ class Solution {
         
     }
 }
+
+/*
+Approach-2:Recursion + Memoization(DP);
+Time complexity:O(n);
+Space complexity:O(n);
+*/
+class Solution {
+    public int fibo(int n,int[]dp){
+        if(n<=1) return n;
+        if(dp[n]!=0) return dp[n];
+        int ans=fibo(n-1,dp)+fibo(n-2,dp);
+        dp[n]=ans;
+        return ans;
+    }
+    public int fib(int n) {
+       int [] dp=new int[n+1];// index from 0 to n
+      return fibo(n,dp);  
+    }
+}
