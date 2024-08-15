f71fc48e1a48f5d7967a51d274a809361728fd88
shivamsinghtomar78 <139133103+shivamsinghtomar78@users.noreply.github.com>
2024-08-15 22:30:29 +0530

    Update House Robber(Leetcode 198).java

--- DP/House Robber(Leetcode 198).java
+++ DP/House Robber(Leetcode 198).java
@@ -19,3 +19,27 @@ class Solution {
         return rob(arr,0,n,dp);
     }
 }
+
+
+
+/*
+Approach-2:Bottom-up dp or iterative dp or tabulation dp;
+Time complexity:O(n);
+Space complexity:O(n);
+*/
+
+class Solution {
+    public int rob(int[] arr) {
+        int n=arr.length;
+        if(n==1) return arr[0];
+        int[] dp=new int[n+1];
+        dp[0]=0;
+        dp[1]=arr[0];
+        for(int i=2;i<=n;i++){
+            int skip=dp[i-1];
+            int rob=dp[i-2]+arr[i-1];
+            dp[i]=Math.max(skip,rob);
+        }
+        return dp[n];
+    }
+}
