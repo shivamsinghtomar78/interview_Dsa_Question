03ea71d9175eab58402144d3d5a54c3de193c3fa
shivamsinghtomar78 <139133103+shivamsinghtomar78@users.noreply.github.com>
2024-08-16 18:27:44 +0530

    Update Climbing Stairs(Leetcode 70).java

--- DP/Climbing Stairs(Leetcode 70).java
+++ DP/Climbing Stairs(Leetcode 70).java
@@ -19,3 +19,22 @@ class Solution {
         return dp[n]=one+two;
     }
 }
+/*
+Approach-2:Bottom-up dp or iterative dp or tabulation dp;
+Time complexity:O(n);
+Space complexity:O(n);
+*/
+
+class Solution {
+    public int climbStairs(int n) {
+        if(n == 1 || n == 2)
+            return n;
+       int []dp=new int[n+1];
+       dp[1]=1;
+       dp[2]= 2;
+       for(int i=3;i<n+1;i++){
+        dp[i]=dp[i-1]+dp[i-2];
+       }
+       return dp[n];
+    }
+}
