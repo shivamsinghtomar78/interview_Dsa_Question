b9d268bb1b1d59363947f9c474d36ad567592241
shivamsinghtomar78 <139133103+shivamsinghtomar78@users.noreply.github.com>
2024-08-16 19:44:06 +0530

    Update Unique paths(Leetcode 62).java

--- DP/Unique paths(Leetcode 62).java
+++ DP/Unique paths(Leetcode 62).java
@@ -22,3 +22,23 @@ class Solution {
         return dp[m][n]= down+right;
     }
 }
+
+/*
+Approach-2:Bottom-up dp or iterative dp or tabulation dp;
+Time complexity:O(m*n);
+Space complexity:O(m*n);
+*/
+
+class Solution {
+    public int uniquePaths(int m, int n) {
+        int [][]dp=new int[m+1][n+1];
+        for(int i=1;i<m+1;i++){
+            for(int j=1;j<n+1;j++){
+                if(i==1 || j==1) dp[i][j]=1;
+                else dp[i][j]=dp[i][j-1]+dp[i-1][j];
+            }
+        }
+        return dp[m][n];
+    }
+}
+
