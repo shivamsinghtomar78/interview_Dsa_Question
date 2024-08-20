0a2c1ba1790c9efe36354ba94aa0e00309b39520
shivamsinghtomar78 <139133103+shivamsinghtomar78@users.noreply.github.com>
2024-08-20 11:45:33 +0530

    Update Perfect Square(Leetcode 279).java

--- DP/Perfect Square(Leetcode 279).java
+++ DP/Perfect Square(Leetcode 279).java
@@ -20,3 +20,32 @@ class Solution {
         return dp[n]=minCount;
     }
 }
+/*
+Approach-2:Bottom-up dp or iterative dp or tabulation dp;
+Time complexity:O(n*sqrt(n));
+Space complexity:O(n);
+*/
+
+class Solution {
+    boolean isPerfect(int n){
+        int sqrt=(int)(Math.sqrt(n));
+        return (sqrt*sqrt==n);
+    }
+    public int numSquares(int n) {
+        int []dp=new int[n+1];
+        for(int i=1;i<=n;i++){
+            if(isPerfect(i)) dp[i]=1;
+            else{
+                int min=Integer.MAX_VALUE;
+                for(int j=1;j*j<=i;j++){
+                    int count=dp[j*j]+dp[i-j*j];
+                    min=Math.min(min,count);
+                }
+                dp[i]=min;
+            }
+            
+        }
+         return dp[n];
+}
+
+}
