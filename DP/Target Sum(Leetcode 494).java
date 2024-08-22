ee5fbf64b89c2951753c18a2ba0e2eecf56ce42a
shivamsinghtomar78 <139133103+shivamsinghtomar78@users.noreply.github.com>
2024-08-22 22:52:41 +0530

    Update Target Sum(Leetcode 494).java

--- DP/Target Sum(Leetcode 494).java
+++ DP/Target Sum(Leetcode 494).java
@@ -18,3 +18,23 @@ class Solution {
          return count;
     }
 }
+/*
+Approach-2:Recurrsion;
+Time complexity:O(2^n);
+Space complexity:O(n);
+*/
+
+class Solution {
+    public int findTargetSumWays(int[] arr, int target) {
+        return ways(0,arr,target);
+    }
+    public int ways(int i,int[]arr,int target){
+        if(i==arr.length){
+            if(target==0) return 1;
+            else return 0;
+        }
+        int add=ways(i+1,arr,target-arr[i]);
+         int sub=ways(i+1,arr,target+arr[i]);
+         return add+sub;
+    }
+}
