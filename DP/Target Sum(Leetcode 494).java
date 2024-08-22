62ebe3dbe0986f16038880c91b1780f9faf3de4f
shivamsinghtomar78 <139133103+shivamsinghtomar78@users.noreply.github.com>
2024-08-22 23:45:24 +0530

    Update Target Sum(Leetcode 494).java

--- DP/Target Sum(Leetcode 494).java
+++ DP/Target Sum(Leetcode 494).java
@@ -38,3 +38,32 @@ class Solution {
          return add+sub;
     }
 }
+/*
+Approach-3:Recurrsion;
+Time complexity:O(n*taregt);
+Space complexity:O(n*target);
+*/
+
+class Solution {
+    int sum;
+    public int findTargetSumWays(int[] arr, int target) {
+        // i--> 0 to n-1 | target =-sum to sum
+        // dp[i][target+sum]
+        sum=0; int n=arr.length;
+        for(int ele:arr) sum+=ele;
+        int[][] dp=new int[n][2*sum+1];
+        for(int i=0;i<dp.length;i++)
+        for(int j=0;j<dp[0].length;j++) dp[i][j]=-1;
+        return ways(0,arr,0,target,dp);
+    }
+    public int ways(int i,int[]arr,int res,int target,int[][]dp){
+        if(i==arr.length){
+            if(res==target) return 1;
+            else return 0;
+        }
+        if(dp[i][res+sum]!=-1) return dp[i][res+sum];
+        int add=ways(i+1,arr,res-arr[i], target,dp);
+         int sub=ways(i+1,arr,res+arr[i], target,dp);
+         return dp[i][res+sum]=add+sub;
+    }
+}
