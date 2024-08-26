915070b4d1b99a565479d153701b046ebbc875b4
shivamsinghtomar78 <139133103+shivamsinghtomar78@users.noreply.github.com>
2024-08-26 22:57:42 +0530

    Update Longest common subsequence(Leetcode 1143).java

--- DP/Longest common subsequence(Leetcode 1143).java
+++ DP/Longest common subsequence(Leetcode 1143).java
@@ -37,20 +37,18 @@ class Solution {
         
         int m = a.length();
         int n = b.length();
-        int[][] dp = new int[m][n];
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                int p = (i>0 && j>0)? dp[i - 1][j - 1]:0;
-                int q = (i>0)? dp[i - 1][j]:0;
-                int r =(j>0)? dp[i][j - 1]:0;
-                if (a.charAt(i) == b.charAt(j)) {
-                    dp[i][j] = 1 + p;
+        int[][] dp = new int[m+1][n+1];
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
+                
+                if (a.charAt(i-1) == b.charAt(j-1)) {
+                    dp[i][j] = 1 + dp[i-1][j-1];
                 } else {
-                    dp[i][j] = Math.max(q, r);
+                    dp[i][j] = Math.max(dp[i-1][j], dp[i][j-1]);
                 }
             }
         }
 
-        return dp[m - 1][n - 1];
+        return dp[m][n];
     }
 }
