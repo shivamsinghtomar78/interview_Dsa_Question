b1d40e789c57c89bacce601683ae88193a5f1548
shivamsinghtomar78 <139133103+shivamsinghtomar78@users.noreply.github.com>
2024-08-26 11:24:16 +0530

    Update coin change(Leetcode 322).java

--- DP/coin change(Leetcode 322).java
+++ DP/coin change(Leetcode 322).java
@@ -29,3 +29,33 @@ class Solution {
 
     }
 }
+
+/*
+Approach-2:Bottom-up dp or iterative dp or tabulation dp;
+Time complexity:O(n*amount);
+Space complexity:O(n*amount);
+*/
+
+class Solution {
+    public int coinChange(int[] coins, int amount) {
+        int n = coins.length;
+        long[][] dp = new long[coins.length][amount + 1];
+        for (int i = 0; i < dp.length; i++) {
+            for (int j = 0; j < dp[0].length; j++) {
+                long skip =(i>0)?dp[i-1][j]:((j==0)?0:Integer.MAX_VALUE);
+                if (j - coins[i] < 0)
+                     dp[i][j] = skip;
+               else{ 
+                long pick=1+dp[i][j-coins[i]];
+             dp[i][j] = Math.min(skip, pick);
+               }
+            }
+        }
+        int ans = (int) dp[n-1][amount];
+        if (ans == Integer.MAX_VALUE)
+            return -1;
+        return ans;
+    }
+
+}
+
