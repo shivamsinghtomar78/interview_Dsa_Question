d1c971da3a87da9c638782770e7c7aaeba385f98
shivamsinghtomar78 <139133103+shivamsinghtomar78@users.noreply.github.com>
2024-08-26 22:29:56 +0530

    Update Longest common subsequence(Leetcode 1143).java

--- DP/Longest common subsequence(Leetcode 1143).java
+++ DP/Longest common subsequence(Leetcode 1143).java
@@ -26,3 +26,31 @@ class Solution {
         return lcs(m-1, n-1, a, b,dp);
     }
 }
+
+/*
+Approach-2:Bottom-up dp or iterative dp or tabulation dp;
+Time complexity:O(n);
+Space complexity:O(n);
+*/
+class Solution {
+    public int longestCommonSubsequence(String a, String b) {
+        
+        int m = a.length();
+        int n = b.length();
+        int[][] dp = new int[m][n];
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                int p = (i>0 && j>0)? dp[i - 1][j - 1]:0;
+                int q = (i>0)? dp[i - 1][j]:0;
+                int r =(j>0)? dp[i][j - 1]:0;
+                if (a.charAt(i) == b.charAt(j)) {
+                    dp[i][j] = 1 + p;
+                } else {
+                    dp[i][j] = Math.max(q, r);
+                }
+            }
+        }
+
+        return dp[m - 1][n - 1];
+    }
+}
