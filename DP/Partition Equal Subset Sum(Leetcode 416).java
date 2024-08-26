f38626725a7147f99ac3dcf26047fa9729e7fc75
shivamsinghtomar78 <139133103+shivamsinghtomar78@users.noreply.github.com>
2024-08-26 10:19:53 +0530

    Update Partition Equal Subset Sum(Leetcode 416).java

--- DP/Partition Equal Subset Sum(Leetcode 416).java
+++ DP/Partition Equal Subset Sum(Leetcode 416).java
@@ -35,3 +35,39 @@ class Solution {
         return ans;
     }
 }
+
+/*
+Approach-2:Bottom-up dp or iterative dp or tabulation dp;
+Time complexity:O(n*target);
+Space complexity:O(n*target);
+*/
+
+class Solution {
+    public boolean canPartition(int[] arr) {
+        int n=arr.length;
+        int sum = 0;
+        for (int ele : arr)
+            sum += ele;
+        if (sum % 2 != 0)
+            return false;
+
+        int target = sum / 2;
+        int[][] dp = new int[arr.length][target + 1];
+        for (int i = 0; i < dp.length; i++) {
+            for (int j = 0; j < dp[0].length; j++) {
+                boolean ans = false;
+                boolean skip =(i>0) ? (dp[i-1][j]==1):(j==0);
+                if (j - arr[i] < 0)
+                    ans = skip;
+                else {
+                    boolean pick =  (i>0) ?(dp[i-1][j-arr[i]]==1):(j==0);
+                    ans = pick || skip;
+                }
+                 dp[i][j]=(ans) ? 1:0;
+            }
+        }
+
+        return dp[n - 1][target] == 1;
+    }
+}
+
