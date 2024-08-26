ff8d5a939e50df1f23bf279c27d7122c42c360b4
shivamsinghtomar78 <139133103+shivamsinghtomar78@users.noreply.github.com>
2024-08-26 23:26:15 +0530

    Update Longest common subsequence(Leetcode 1143).java

--- DP/Longest common subsequence(Leetcode 1143).java
+++ DP/Longest common subsequence(Leetcode 1143).java
@@ -29,8 +29,8 @@ class Solution {
 
 /*
 Approach-2:Bottom-up dp or iterative dp or tabulation dp;
-Time complexity:O(n);
-Space complexity:O(n);
+Time complexity:O(m*n);
+Space complexity:O(m*n);
 */
 class Solution {
     public int longestCommonSubsequence(String a, String b) {
@@ -50,5 +50,34 @@ class Solution {
         }
 
         return dp[m][n];
+    }
+}
+
+/*
+Approach-3:Bottom-up dp or iterative dp or tabulation dp;
+Time complexity:O(m*n);
+Space complexity:O(n);
+*/
+
+class Solution {
+    public int longestCommonSubsequence(String a, String b) {
+        
+        int m = a.length();
+        int n = b.length();
+        int[][] dp = new int[2][n+1];
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
+                
+                if (a.charAt(i-1) == b.charAt(j-1)) {
+                    dp[1][j] = 1 + dp[0][j-1];
+                } else {
+                    dp[1][j] = Math.max(dp[0][j], dp[1][j-1]);
+                }
+            }
+            // copy the first row to 0th row for next time
+            for(int j=0;j<=n;j++) dp[0][j]=dp[1][j];
+        }
+
+        return dp[1][n];
     }
 }
