2779203ed6d2cb3f4a7706a01e7105784d410a8a
shivamsinghtomar78 <139133103+shivamsinghtomar78@users.noreply.github.com>
2024-09-10 20:01:34 +0530

    Create 2. Greatest Common Divisor of Strings(Leetcode 1071).java

---
+++ LeetCode 75/2. Greatest Common Divisor of Strings(Leetcode 1071).java
@@ -0,0 +1,18 @@
+/*
+Approach: Three poiner;
+Time complexity:O(M+N);
+Space complexity:O(M+N); */
+
+class Solution {
+    public String gcdOfStrings(String str1, String str2) {
+        
+        if (!(str1 + str2).equals(str2 + str1))
+            return "";
+        int gcd = gcd(str1.length(), str2.length());
+        return str1.substring(0, gcd);
+    }
+
+    private int gcd(int a, int b) {
+        return b == 0 ? a : gcd(b, a % b);
+    }
+}
