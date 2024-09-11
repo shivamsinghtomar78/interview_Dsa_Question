aaad74b5164cae2161144cc08209b6bd351da30c
shivamsinghtomar78 <139133103+shivamsinghtomar78@users.noreply.github.com>
2024-09-12 01:35:04 +0530

    Update 6.Reverse Words in a String(Leetcode 151).java

--- LeetCode 75/6.Reverse Words in a String(Leetcode 151).java
+++ LeetCode 75/6.Reverse Words in a String(Leetcode 151).java
@@ -33,3 +33,59 @@ class Solution {
     }
 }
 
+
+/*
+Approach: Two poiner;
+Time complexity:O(N);
+Space complexity:O(N); */
+
+class Solution {
+    public String reverseWords(String s) {
+        char[] charArray = s.toCharArray();
+        
+        // 1. Reverse the whole string
+        reverse(charArray, 0, charArray.length - 1);
+        
+        int i = 0;
+        int l = 0, r = 0;
+        int n = charArray.length;
+        
+        while (i < n) {
+            // Move non-space characters
+            while (i < n && charArray[i] != ' ') {
+                charArray[r] = charArray[i];
+                r++;
+                i++;
+            }
+            
+            if (l < r) {
+                // Reverse the word
+                reverse(charArray, l, r - 1);
+                
+                // Add a space after the word
+                if (r < n) {
+                    charArray[r++] = ' ';
+                }
+                
+                l = r;
+            }
+            
+            i++;
+        }
+        
+        // Trim the trailing spaces
+        while (r > 0 && charArray[r - 1] == ' ') {
+            r--;
+        }
+        
+        return new String(charArray, 0, r);
+    }
+    
+    private void reverse(char[] s, int left, int right) {
+        while (left < right) {
+            char temp = s[left];
+            s[left++] = s[right];
+            s[right--] = temp;
+        }
+    }
+}
