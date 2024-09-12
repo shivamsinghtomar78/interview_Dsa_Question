9e2cda3217c12ea0dd07f8ca4dae7473b191ada5
shivamsinghtomar78 <139133103+shivamsinghtomar78@users.noreply.github.com>
2024-09-12 23:10:48 +0530

    Create 8.Increasing Triplet Subsequence(Leetcode 334).java

---
+++ LeetCode 75/8.Increasing Triplet Subsequence(Leetcode 334).java
@@ -0,0 +1,22 @@
+/*
+Approach: Two poiner;
+Time complexity:O(N);
+Space complexity:O(1); */
+class Solution {
+    public boolean increasingTriplet(int[] nums) {
+        int first = Integer.MAX_VALUE;
+        int second = Integer.MAX_VALUE;
+        
+        for (int num : nums) {
+            if (num <= first) {
+                first = num;
+            } else if (num <= second) {
+                second = num;
+            } else {
+                return true;
+            }
+        }
+        
+        return false;
+    }
+} 
