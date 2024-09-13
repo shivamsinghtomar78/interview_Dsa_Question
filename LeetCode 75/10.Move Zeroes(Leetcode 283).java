3e4aaa74b9d4f62518a80074cc744b6943680033
shivamsinghtomar78 <139133103+shivamsinghtomar78@users.noreply.github.com>
2024-09-13 21:58:51 +0530

    Update 10.Move Zeroes(Leetcode 283).java

--- LeetCode 75/10.Move Zeroes(Leetcode 283).java
+++ LeetCode 75/10.Move Zeroes(Leetcode 283).java
@@ -24,3 +24,45 @@ class Solution {
     }
 }
 
+/*
+Approach: Two pointer;
+Time complexity:O(N);
+Space complexity:O(1); */
+
+
+
+
+class Solution {
+    public void swap(int[] arr,int i,int j){
+        int temp=arr[i];
+        arr[i]=arr[j];
+        arr[j]=temp;
+    }
+    public void moveZeroes(int[] nums) {
+        int n=nums.length;
+        int count=0;
+        for(int i=0;i<n;i++){
+            if(nums[i]==0) count++;
+        }
+        int i=0;
+        int j=0;
+        while(i<n && j<n){
+            if( nums[i]==0 && nums[j]!=0 ){
+                swap(nums,i,j);
+                i++;
+                j++;
+            }
+            else if( nums[i]==0 && nums[j]==0 ){
+               
+                j++;
+             }
+
+            else {
+                i++;
+                j++;
+            }
+             
+        }
+    }
+}
+
