ef1f191baf456dba9ff50ac6dfd4e72eb2b4b62a
shivamsinghtomar78 <139133103+shivamsinghtomar78@users.noreply.github.com>
2024-09-16 12:26:37 +0530

    Update 17.Longest Subarray of 1's After Deleting One Element(Leetcode 1493).java

--- LeetCode 75/17.Longest Subarray of 1's After Deleting One Element(Leetcode 1493).java
+++ LeetCode 75/17.Longest Subarray of 1's After Deleting One Element(Leetcode 1493).java
@@ -24,3 +24,51 @@ class Solution {
         return mx;
     }
 }
+
+/*
+Approach: sliding window and two pointer  ;
+Time complexity:O(N);
+Space complexity:O(1); */
+
+
+class Solution {
+    public int longestSubarray(int[] arr) {
+        int n=arr.length;
+        int zeroes=0;
+        int len=0;
+        int i=0;
+        for(int j=0;j<n;j++){
+            if(arr[j]==0) zeroes++;
+            while(zeroes>1){
+                if(arr[i]==0) zeroes--;
+                i++;
+            }
+            len=Math.max(len,j-i);
+        }
+        return len;
+    }
+}
+
+/*
+Approach: sliding window and two pointer  ;
+Time complexity:O(N);
+Space complexity:O(1); */
+
+
+class Solution {
+    public int longestSubarray(int[] arr) {
+        int i=0,j=0;
+        int n=arr.length;
+        int last_zeroes_idx=-1;
+        int result=0;
+        while(j<n){
+            if(arr[j]==0){
+                i= last_zeroes_idx+1;
+                 last_zeroes_idx=j;
+            }
+            result=Math.max(result,j-i);
+            j++;
+        }
+        return result;
+    }
+}
