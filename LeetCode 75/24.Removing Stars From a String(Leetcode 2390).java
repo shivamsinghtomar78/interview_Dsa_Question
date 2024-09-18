8f92c9b9bf1b3ba8ad8fe66cd2e86e1dac83adc5
shivamsinghtomar78 <139133103+shivamsinghtomar78@users.noreply.github.com>
2024-09-19 00:27:54 +0530

    Update 24.Removing Stars From a String(Leetcode 2390).java

--- LeetCode 75/24.Removing Stars From a String(Leetcode 2390).java
+++ LeetCode 75/24.Removing Stars From a String(Leetcode 2390).java
@@ -35,3 +35,28 @@ class Solution {
         
     }
 }
+/*
+Approach:Stack ;
+Time complexity:O(N);
+Space complexity:O(N); */
+
+
+class Solution {
+    public String removeStars(String s) {
+        Stack<Character> st=new Stack<>();
+        for(int i=0;i<s.length();i++){
+            if(s.charAt(i)!='*'){
+                st.push(s.charAt(i));
+            }
+            else st.pop();
+        }
+       StringBuilder ans=new StringBuilder();
+       int n=st.size();
+
+        for(int i=0;i<n;i++){
+            ans.append(st.pop());
+        }
+        return ans.reverse().toString();
+    }
+}
+
