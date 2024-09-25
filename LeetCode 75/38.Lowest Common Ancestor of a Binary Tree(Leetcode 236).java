a916cb376cadda2006256a7daa478d233d1401e3
shivamsinghtomar78 <139133103+shivamsinghtomar78@users.noreply.github.com>
2024-09-25 22:22:51 +0530

    Update 38.Lowest Common Ancestor of a Binary Tree(Leetcode 236).java

--- LeetCode 75/38.Lowest Common Ancestor of a Binary Tree(Leetcode 236).java
+++ LeetCode 75/38.Lowest Common Ancestor of a Binary Tree(Leetcode 236).java
@@ -25,3 +25,26 @@ class Solution {
             return root;
     }
 }
+
+
+/*
+Approach: Binary tree;
+Time complexity:O(N);
+Space complexity:O(N); */
+class Solution {
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if (root == null)
+            return null;
+        
+        if (root.val == p.val || root.val == q.val)
+            return root;
+        
+        TreeNode l = lowestCommonAncestor(root.left, p, q);
+        TreeNode r = lowestCommonAncestor(root.right, p, q);
+        
+        if (l != null && r != null)
+            return root;
+        
+        return (l != null) ? l : r;
+    }
+}
