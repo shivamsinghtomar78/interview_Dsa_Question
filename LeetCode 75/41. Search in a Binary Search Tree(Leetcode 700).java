1724dc0bb17d089eba8a4c304a46de57021e9ad2
shivamsinghtomar78 <139133103+shivamsinghtomar78@users.noreply.github.com>
2024-09-30 10:09:37 +0530

    Create 41. Search in a Binary Search Tree(Leetcode 700).java

---
+++ LeetCode 75/41. Search in a Binary Search Tree(Leetcode 700).java
@@ -0,0 +1,14 @@
+/*
+Approach: Binary Search tree:
+Time complexity:O(H);
+Space complexity:O(H); */
+
+class Solution {
+    public TreeNode searchBST(TreeNode root, int val) {
+        if(root==null) return null;
+        if(root.val<val) return searchBST(root.right,val);
+        else if(root.val>val) return searchBST( root.left, val);
+        else return root;
+       
+}
+}
