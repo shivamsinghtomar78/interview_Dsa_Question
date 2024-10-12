dc970803121c9f1151fa89af45a3d1760b4bfa1e
shivamsinghtomar78 <139133103+shivamsinghtomar78@users.noreply.github.com>
2024-10-12 11:53:06 +0530

    Create Merge Sort.java

---
+++ Recursion/Merge Sort.java
@@ -0,0 +1,66 @@
+/*Time complexity:O(nLogn);
+Space complexity:O(n);
+Problem:https://www.geeksforgeeks.org/problems/merge-sort/1
+*/
+
+
+class Solution {
+    void merge(int arr[], int l, int m, int r) {
+        int n1 = m - l + 1;
+        int n2 = r - m;
+
+        // Create temporary arrays to store the two halves
+        int L[] = new int[n1];
+        int R[] = new int[n2];
+        int k   = l;
+        // Copy data to temporary arrays L[] and R[]
+        for (int i = 0; i < n1; i++)
+            L[i] = arr[k++];
+        for (int j = 0; j < n2; j++)
+            R[j] = arr[k++];
+
+        // Merge the temporary arrays back into arr[l..r]
+        int i = 0; // Initial index of first subarray
+        int j = 0; // Initial index of second subarray
+        k = l; // Initial index of merged subarray
+
+        while (i < n1 && j < n2) {
+            if (L[i] <= R[j]) {
+                arr[k] = L[i];
+                i++;
+            } else {
+                arr[k] = R[j];
+                j++;
+            }
+            k++;
+        }
+
+        // Copy the remaining elements of L[], if there are any
+        while (i < n1) {
+            arr[k] = L[i];
+            i++;
+            k++;
+        }
+
+        // Copy the remaining elements of R[], if there are any
+        while (j < n2) {
+            arr[k] = R[j];
+            j++;
+            k++;
+        }
+    }
+
+    void mergeSort(int arr[], int l, int r) {
+
+        if (l >= r) {
+            return;
+        }
+
+        int mid = l + (r - l) / 2;
+
+        mergeSort(arr, l, mid);
+        mergeSort(arr, mid + 1, r);
+
+        merge(arr, l, mid, r);
+    }
+}
