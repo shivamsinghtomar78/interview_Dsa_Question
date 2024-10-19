dd4b456b6da35ce28262d34247c5edacb0cadfcd
shivamsinghtomar78 <139133103+shivamsinghtomar78@users.noreply.github.com>
2024-10-19 22:27:15 +0530

    Update Minimum Bit Flips to Convert Number(Leetcode 2220).java

--- Bit Manipulation/Minimum Bit Flips to Convert Number(Leetcode 2220).java
+++ Bit Manipulation/Minimum Bit Flips to Convert Number(Leetcode 2220).java
@@ -12,3 +12,23 @@ class Solution {
         return Integer.bitCount(xor);
     }
 }
+
+
+/*
+Aprroach:using Xor and Right most 1;
+Time complexity:O(1);
+Space complexity:O(1);
+*/
+
+class Solution {
+    public int minBitFlips(int start, int goal) {
+        // XOR will give 1 only where bits differ
+        int xor = start ^ goal;
+       int count=0;
+        while(xor!=0){
+            xor=xor&(xor-1);
+            count++;
+        }
+        return count;
+    }
+}
