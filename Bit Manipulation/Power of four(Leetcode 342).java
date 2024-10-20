04a701858c661f8692905572f3da437683f207af
shivamsinghtomar78 <139133103+shivamsinghtomar78@users.noreply.github.com>
2024-10-20 21:49:00 +0530

    Create Power of four(Leetcode 342).java

---
+++ Bit Manipulation/Power of four(Leetcode 342).java
@@ -0,0 +1,38 @@
+/*
+Approach:Brute force;
+Time complexity:O(logN);
+Space complexity:O(1);
+*/
+
+class Solution {
+    public boolean isPowerOfFour(int n) {
+        if(n<=0) return false;
+        if(n==1) return true;
+
+        while(n>1){
+            if(n%4!=0) return false;
+            n=n/4;
+        }
+        return true;
+
+    }
+}
+
+
+/*
+Approach:Brute force;
+Time complexity:O(1);
+Space complexity:O(1);
+*/
+
+
+class Solution {
+    public boolean isPowerOfFour(int n) {
+        if(n<=0) return false;
+        return ((n&(n-1))==0) && isPerfectSquare(n);
+    }
+    public boolean isPerfectSquare(int n){
+        double root=Math.sqrt(n);
+        return root*root==n;
+    }
+}
