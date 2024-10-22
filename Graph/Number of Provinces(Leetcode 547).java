76d9b1ebdd0f0e33e8991b556b8d67c9221d7abf
shivamsinghtomar78 <139133103+shivamsinghtomar78@users.noreply.github.com>
2024-10-22 10:57:18 +0530

    Create Number of Provinces(Leetcode 547).java

---
+++ Graph/Number of Provinces(Leetcode 547).java
@@ -0,0 +1,39 @@
+/*
+Appraoch:using BFS  ;
+Time Complexity:O(N^2);
+Space Complexity:O(N)
+
+  */
+
+class Solution {
+    public int findCircleNum(int[][] adj) {
+        int  n=adj.length;
+        int count=0;
+        boolean[] vis=new boolean[n];
+        for(int i=0;i<n;i++){
+            if(!vis[i]){
+                bfs(i,vis,adj);
+                count++;
+            }
+        }
+        return count;
+    }
+    private void bfs (int i,boolean[] vis ,int[][] adj){
+        int n=adj.length;
+        vis[i]=true;
+        Queue<Integer> q=new LinkedList<>();
+            q.add(i);
+            while(q.size()>0){
+                int front=q.remove();
+                for(int j=0;j<n;j++){
+                    if(adj[front][j]==1 && vis[j]==false){
+                        q.add(j);
+                        vis[j]=true;
+                    }
+                }
+            }
+    }
+}
+  
+
+
