c099bdba836628e344a1d496702782938a016245
shivamsinghtomar78 <139133103+shivamsinghtomar78@users.noreply.github.com>
2024-10-23 23:38:37 +0530

    Update Keys and Rooms(Leetcode 841).java

--- Graph/Keys and Rooms(Leetcode 841).java
+++ Graph/Keys and Rooms(Leetcode 841).java
@@ -32,3 +32,33 @@ class Solution {
         }
     }
 }
+
+
+/*
+Appraoch:using BFS  ;
+Time Complexity: O(n + m)
+Space Complexity: O(n + m)
+
+  */
+
+
+
+class Solution {
+    public boolean canVisitAllRooms(List<List<Integer>> adj) {
+        int n=adj.size();
+        boolean[] visited=new boolean[n];
+        visited[0]=true;
+        dfs(0,adj,visited);
+        for(boolean ele:visited){
+            if(ele==false) return false;
+        }
+        return true;
+
+    }
+     private void dfs(int start,List<List<Integer>> adj,boolean[] visited){
+        visited[start]=true;
+        for(int ele:adj.get(start)){
+        if(!visited[ele]) dfs(ele,adj,visited);
+        }
+     }
+}
