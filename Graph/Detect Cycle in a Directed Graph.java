16077e7def175dccf81f3f85422bbd59929f0256
shivamsinghtomar78 <139133103+shivamsinghtomar78@users.noreply.github.com>
2024-10-28 18:01:34 +0530

    Update Detect Cycle in a Directed Graph.java

--- Graph/Detect Cycle in a Directed Graph.java
+++ Graph/Detect Cycle in a Directed Graph.java
@@ -33,3 +33,57 @@ class Solution {
         return false;
     }
 }
+
+
+
+/*
+Aprroach:Using BFS;
+Time Complexity:O(V+E);
+Space Complexity:O(V);
+Question Link : https://www.geeksforgeeks.org/problems/detect-cycle-in-a-directed-graph/1
+*/
+
+
+
+class Solution {
+    // Function to detect cycle in a directed graph.
+    public boolean isCyclic(int N, ArrayList<ArrayList<Integer>> adj) {
+        Queue<Integer> q=new LinkedList<>();
+        
+        int []indegree=new int[N];
+        for(int u=0;u<N;u++){
+            for(int v:adj.get(u)){
+                indegree[v]++;
+            }
+        }
+        
+        
+        int count=0;
+        for(int i=0;i<N;i++){
+            if(indegree[i]==0){
+                q.add(i);
+                count++;
+            }
+        }
+        
+        while(!q.isEmpty()){
+            int u=q.remove();
+            
+            
+            for(int v:adj.get(u)){
+                indegree[v]--;
+                
+                if(indegree[v]==0){
+                    q.add(v);
+                    count++;
+                }
+            }
+        }
+        
+        
+        if(count==N) return false;// we visited all the node ,mtlb no cycle
+        else return true;
+        
+        
+    }
+}
